d7745402a03557d8d5d9b763429d2f608665431e
olman2020 <irodriguezcr1@gmail.com>
2021-08-26 15:53:02 -0600

    Correccion del Selection Sort

--- Trabajo-Extra-Clase_1/selection_sort.h
+++ Trabajo-Extra-Clase_1/selection_sort.h
@@ -6,6 +6,9 @@ class Selection_Sort
 {
 public:
     Selection_Sort();
+    void swap(int *xp, int *yp);
+    void selectionSort(int arr[], int n);
+    void printArray(int arr[], int size);
 };
 
 #endif // SELECTION_SORT_H
